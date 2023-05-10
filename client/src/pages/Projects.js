4cfafc574b545371343fb97328a0be9968b208d1
Leon Plackal <110382152+leon-plackal@users.noreply.github.com>
2023-05-10 16:16:54 +1000

    Added project

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -2,9 +2,12 @@ import {Link} from "react-router-dom"
 import image2 from "../assets/KeyNinja.png"
 import image1 from "../assets/portfoliov1.png"
 import image3 from "../assets/denoiser_thumb.png"
+import ArtiGenTmb from "../assets/ArtiGenThumb.png"
 import { AiFillGithub } from 'react-icons/ai';
 import { AiFillLinkedin } from 'react-icons/ai';
 import { GiPlainCircle } from 'react-icons/gi';
+import { BiLink } from 'react-icons/bi';
+
 
 function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
@@ -49,7 +52,39 @@ export default function Projects() {
         <div id="project-header">
             <p className="subheading">My Latest Work</p>
             <h1 class="title">0.2 Recent Projects</h1>
-
+            {/* ArtiGen */}
+            <div class="featured-project">
+                <div class="image">
+                <img src={ArtiGenTmb} alt="Portfolio V1" />
+                </div>
+                <div class="info">
+                    <h3>Featured Project</h3>
+                    <h2>ArtiGen AI Art App</h2>
+                    <div class="featured-description">
+                        <p>This website makes use of the OpenAI API to allow users to create their own artworks via a prompt and have the choice to add it to my community showcase for everyone to see. The site utilizes the Vite framework and additionally MongoDB, Tailwind CSS, Express and cloud image hosting. 
+                        The app is deployed with Vercel and Render.
+                        </p>
+                    </div>
+                    <div class="tags">
+                        <ul>
+                            <li>Vite</li>
+                            <li>TailwindCSS</li>
+                            <li>Cloudinary</li>
+                            <li>ReactJS</li>
+                            <li>Vercel</li>
+                        </ul>
+                        <div id="taglink">
+                            <a href='https://artigenai.vercel.app/'>
+                                <BiLink id='socials-icon' />
+                            </a>
+                            <a href='https://github.com/leon-plackal'>
+                                <AiFillGithub id='socials-icon' />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* KeyNinja */}
             <div class="featured-project">
                 <div class="image">
                 <img src={image2} alt="KeyNinja" />
@@ -67,6 +102,9 @@ export default function Projects() {
                             <li>HTML</li>
                         </ul>
                         <div id="taglink">
+                            <a href='https://keyninjatype.netlify.app/'>
+                                <BiLink id='socials-icon' />
+                            </a>
                             <a href='https://github.com/leon-plackal'>
                                 <AiFillGithub id='socials-icon' />
                             </a>
@@ -75,35 +113,12 @@ export default function Projects() {
                     
                 </div>
             </div>
-            <div class="featured-project">
-                <div class="image">
-                <img src={image1} alt="Portfolio V1" />
-                </div>
-                <div class="info">
-                    <h3>Featured Project</h3>
-                    <h2>Portfolio V1</h2>
-                    <div class="featured-description">
-                        <p>The first iteration of my Portfolio built with React and featuring a ThreeJS particle background. It also makes use of routing, light and dark mode, and is hosted on GitHub pages.</p>
-                    </div>
-                    <div class="tags">
-                        <ul>
-                            <li>JavaScript</li>
-                            <li>CSS</li>
-                            <li>HTML</li>
-                            <li>ReactJS</li>
-                        </ul>
-                        <div id="taglink">
-                            <a href='https://github.com/leon-plackal'>
-                                <AiFillGithub id='socials-icon' />
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
 
             <h3 className="subheading">Project Archive</h3>
             <div class="archive-tiles">
                 <div className="tile-row">
+                    
                         <div class="tile">
                             <div class="archiveinfo">
                                 <h6>Simple Image Denoiser</h6>
@@ -116,8 +131,19 @@ export default function Projects() {
                                 <img src={image3} alt="xxxx"/>
                             </div>
                         </div>
-
-                        <div class="tile"></div>
+                    
+                        <div class="tile">
+                            <div class="archiveinfo">
+                                <h6>Portfolio V1</h6>
+                                <p>The first iteration of my Portfolio built with React</p>
+                            </div>
+                            <a href='https://github.com/leon-plackal'>
+                                        <AiFillGithub id='socials-icon2' />
+                            </a>
+                            <div class="archiveimage">
+                                <img src={image1} alt="xxxx"/>
+                            </div>
+                        </div>
                         <div class="tile"></div>
                 </div>
                 <div className="tile-row">
